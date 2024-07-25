63d8dfcef01d0721cada34a8da70f0e37b8cb800
Vadim Shikhovtsov <5208643@mail.ru>
2024-07-25 16:15:53 +0300

    Done. Edit snippet

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import get_object_or_404, render, redirect
 from MainApp.forms import SnippetForm
 from MainApp.models import Snippet
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index_page(request):
@@ -45,11 +46,38 @@ def snippet_detail(request, snippet_id):
         return render(request, "pages/errors.html", context | {"error": f"Snippet with id={snippet_id} not found."})
     else:
         context["snippet"] = snippet
+        context["type"] = "view"
         return render(request, "pages/snippet_detail.html", context)
 
 
 def snippet_edit(request, snippet_id):
-    pass
+    context = {'pagename': 'Редактирование сниппета'}
+    try:
+        snippet = Snippet.objects.get(pk=snippet_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    
+    # Variant 1 с помощью SnippetForm
+    # if request.method == "GET":
+    #     form = SnippetForm(instance=snippet)
+    #     return render(request,"pages/add_snippet.html", {"form": form})
+    
+    # Variant 2
+    # Хотим получить страницу с данными сниппета
+    if request.method == "GET":
+        context = {
+            "snippet": snippet,
+            "type": "edit"
+        }
+        return render(request, "pages/snippet_detail.html", context)
+    
+    # Получаем данные из формы и на их основе меняем значения Сниппета
+    if request.method == "POST":
+        data_form = request.POST
+        snippet.name = data_form["name"]
+        snippet.code = data_form["code"]
+        snippet.save()
+        return redirect("snippets-list")
 
 
 def snippet_delete(request, snippet_id):
