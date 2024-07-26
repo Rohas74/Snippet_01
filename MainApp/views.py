96a44109461e657ae6109ee1d38441744525eaba
Rohas74 <rohas74@yandex.ru>
2024-07-26 11:01:17 +0300

    Day 5 Snippets login requirement

--- MainApp/views.py
+++ MainApp/views.py
@@ -4,13 +4,13 @@ from MainApp.forms import SnippetForm
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import auth
-
+from django.contrib.auth.decorators import login_required
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
-
+@login_required(login_url='home')
 def add_snippet_page(request):
     # Создаем пустую форму при запросе GET
     if request.method == "GET":
